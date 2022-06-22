ff4d61731dcaafb1df4a4d942faf9a22a33bc8f1
Matheus Balcky <matheuscabal821@gmail.com>
2022-06-22 16:58:13 -0300

    Validating name by Joi, and If the name already exist

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ import cors from 'cors';
 import dotenv from 'dotenv'; dotenv.config();
 import chalk from 'chalk';
 import dayjs from 'dayjs';
+import Joi from 'joi';
 import { MongoClient } from 'mongodb';
+
+// & SERVER CONFIG 
 
 const server = express();
 server.use(express.json(), cors());
@@ -11,6 +14,9 @@ server.use(express.json(), cors());
 const client = new MongoClient(process.env.MONGO_URI);
 let db;
 client.connect().then( () => db = client.db("test") )
+
+
+
 
 
 server.get('/participants', (req, res) =>{
@@ -23,14 +29,35 @@ server.get('/participants', (req, res) =>{
 
 
 server.post('/participants', (req, res) =>{
-    const lastStatus = '';
-    const user = {
-        ...req.body,
-        lastStatus,
-    }
-    
-    const promiseInsert = db.collection('participants').insertOne(user);
-    promiseInsert.then( () => {res.status(201).send('created'); console.log('User Logged')} )
+    const { error } = nameSchema.validate(req.body);
+    if(error){
+        return res.status(422).send('Preencha o campo nome corretamente!')
+    };
+
+
+    const ifAlreadyExist = db.collection('participants').find(req.body).toArray();
+    ifAlreadyExist
+    .then( response => {
+
+        if(response.length > 0){
+            console.log('aq')
+            res.status(409).send('Nome já existente tente outro por favor!')
+            return
+        } else {
+            const lastStatus = '';
+            const userDocumment = {
+                ...req.body,
+                lastStatus,
+            }
+        
+            const promiseInsert = db.collection('participants').insertOne(userDocumment);
+            promiseInsert.then( () => {
+                res.status(201).send('created'); console.log('User Logged')
+            })
+        }
+
+    });
+
 });
 
 
@@ -51,9 +78,20 @@ server.post('/messages', (req, res) =>{
     };
     
     const promiseInsertMessage = db.collection('messages').insertOne(message);
-    promiseInsertMessage.then( () => {res.status(201).send('created'); console.log('Message sended to API')} )
-})
+    promiseInsertMessage.then( () => { 
+        res.status(201).send('created');
+        console.log('Message sended to API')
+    })
+});
 
+
+
+
+// & SCHEMA'S VALIDATION
+
+const nameSchema = Joi.object({
+    name: Joi.string().min(3).required(),
+});
 
 
 
